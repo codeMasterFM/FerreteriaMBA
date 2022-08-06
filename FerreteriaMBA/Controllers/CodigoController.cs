1c8f72007bcf0d805dd34564795b931fd3bcccc0
codeMasterFM <franklynp87@gmail.com>
2022-08-05 21:21:36 -0400

    first comit

---
+++ FerreteriaMBA/Controllers/CodigoController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FerreteriaMBA.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace FerreteriaMBA.Controllers
+{
+    public class CodigoController : Controller
+    {
+        private readonly FerreteriaMBA1Context _con;
+
+        public CodigoController(FerreteriaMBA1Context con) {
+            _con = con;
+        }
+        public ActionResult Index()
+        {
+            double total = 0;
+            string codigo = "";
+            double axtoatl = 0;
+            var presultado = _con.Codigos.Include(c => c.IdProductoNavigation).Where(d => d.Estatus == "true").ToList();
+ 
+
+            foreach (var item in presultado)
+            {
+
+                codigo = item.Codigo1;
+
+            }
+
+            var resultado = _con.Codigos.Include(c => c.IdProductoNavigation).Where(d => d.Estatus == "true" && d.Codigo1 == codigo).ToList();
+
+            foreach (var item in resultado)
+            {
+
+                axtoatl = (double)(int.Parse(item.Cantidad) * item.IdProductoNavigation.Precio);
+                total = total + axtoatl;
+            }
+
+                ViewBag.codigo = codigo;
+            ViewBag.Total=  total.ToString("RD$ 0,0.0");
+            
+
+
+            return View( resultado.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Add(string? codigo, string cantidad) {
+
+            var codigos = _con.Productos.Where(d => d.Codigo == codigo).ToList();
+            int? id_product = 0;
+
+            
+            foreach (var item in codigos) {
+
+                id_product = item.IdProducto;
+            }
+            
+           
+            Codigo codigo1 = new Codigo();
+            codigo1.Codigo1 = general_codigo();
+            codigo1.IdProducto = id_product;
+            codigo1.Estatus = "true";
+            codigo1.Cantidad = cantidad;
+            _con.Add(codigo1);
+            _con.SaveChanges();
+
+
+
+
+            return View();
+        }
+
+   
+
+        public string general_codigo()
+        {
+            string codigo = "";
+            string rcodigo = "";
+            var resultado = _con.Codigos.Where(d => d.Estatus == "true").ToList();
+            foreach (var item in resultado)
+            {
+
+                rcodigo = item.Codigo1;
+                
+
+            }
+            if (rcodigo == null)
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    codigo += letras_aleatorias();
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                    codigo += numeros_aleatorias();
+                }
+
+            }
+            else {
+
+                codigo = rcodigo;
+
+            
+            }
+
+            return codigo;
+
+            Console.WriteLine(codigo);
+        }
+        #region general codigos
+        public string letras_aleatorias()
+        {
+            Random random = new Random();
+            List<string> lista = new List<string>() {
+            "a","b","c","d","e","r","z","x","v","b"
+            };
+
+            return lista[random.Next(0, 10)].ToUpper();
+        }
+
+        public string numeros_aleatorias()
+        {
+            Random random = new Random();
+            List<string> lista = new List<string>() {
+            "1","2","3","4","5","6","7","8","9","0"
+            };
+
+            return lista[random.Next(0, 10)];
+        }
+        #endregion
+
+    }
+}
+
+
---
+++ FerreteriaMBA/Controllers/ProductoesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FerreteriaMBA.Models;
+
+namespace FerreteriaMBA.Controllers
+{
+    public class ProductoesController : Controller
+    {
+        private readonly FerreteriaMBA1Context _context;
+
+        public ProductoesController(FerreteriaMBA1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Productoes
+        public async Task<IActionResult> Index()
+        {
+            var ferreteriaMBA1Context = _context.Productos.Include(p => p.IdCategoriaNavigation);
+            return View(await ferreteriaMBA1Context.ToListAsync());
+        }
+
+        // GET: Productoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.IdCategoriaNavigation)
+                .FirstOrDefaultAsync(m => m.IdProducto == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Productoes/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "Nombre");
+            return View();
+        }
+
+        // POST: Productoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProducto,Nombre,Precio,Cantidad,Codigo,IdCategoria")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "Nombre", producto.IdCategoria);
+            return View(producto);
+        }
+
+        // GET: Productoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "Nombre", producto.IdCategoria);
+            return View(producto);
+        }
+
+        // POST: Productoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProducto,Nombre,Precio,Cantidad,Codigo,IdCategoria")] Producto producto)
+        {
+            if (id != producto.IdProducto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.IdProducto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "Nombre", producto.IdCategoria);
+            return View(producto);
+        }
+
+        // GET: Productoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.IdCategoriaNavigation)
+                .FirstOrDefaultAsync(m => m.IdProducto == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Productoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Productos == null)
+            {
+                return Problem("Entity set 'FerreteriaMBA1Context.Productos'  is null.");
+            }
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExists(int id)
+        {
+          return (_context.Productos?.Any(e => e.IdProducto == id)).GetValueOrDefault();
+        }
+    }
+}
